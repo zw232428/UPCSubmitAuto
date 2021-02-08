2bdd5d45e3daea600e622931fde74e3dad47def0
DrS1X <70333032+DrS1X@users.noreply.github.com>
2021-02-08 14:06:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jan 30 10:26:25 2021
-Author WarCock
-"""
-
-signIn = {'username': 'Z20160119', 'password': '190013'}
-old_geo_api_info = '{"type":"complete","info":"SUCCESS","status":1,"$Da":"jsonp_719041_","position":{"Q":21.66355,"R":109.15258,"lng":109.15258,"lat":21.66355},"message":"Get+geolocation+time+out.Get+ipLocation+success.Get+address+success.","location_type":"ip","accuracy":null,"isConverted":true,"addressComponent":{"citycode":"0779","adcode":"450521","businessAreas":[],"neighborhoodType":"","neighborhood":"","building":"","buildingType":"","street":"兴江大道","streetNumber":"103乡","country":"中国","province":"广西壮族自治区","city":"北海市","district":"合浦县","township":"党江镇"},"formattedAddress":"广西壮族自治区北海市合浦县党江镇兴江大道","roads":[],"crosses":[],"pois":[]}'
-
+###
+signIn = {'username': 'Z20160119', #学号
+          'password': '190013'} #登陆密码
+###
 import requests
 import json
 import lxml.html
 import re
+import configparser
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.146 Mobile Safari/537.36',
+}
 
 conn = requests.Session()
 signInResponse= conn.post(
     url="https://app.upc.edu.cn/uc/wap/login/check",
-    data= signIn
+    headers=headers,
+    data= signIn, 
+    timeout=20
 )
 
 historyResponse = conn.get(
     url="https://app.upc.edu.cn/ncov/wap/default/index?from=history",
+    headers=headers,
     data={'from': 'history'}
 )
+historyResponse.encoding = "UTF-8"
 
 html = lxml.html.fromstring(historyResponse.text)
 JS = html.xpath('/html/body/script[@type="text/javascript"]')
@@ -47,11 +51,12 @@ dic['ismoved'] = '0'
 for j in ["date","created","id","gwszdd","sfyqjzgc","jrsfqzys","jrsfqzfy"]:
     dic[j] = defdic[j]
 dic['szgjcs'] = lastParam
-dic['geo_api_info'] = old_geo_api_info
 
 saveResponse = conn.post(
     url="https://app.upc.edu.cn/ncov/wap/default/save",
+    headers=headers,
     data = dic
 )
 
-print(saveResponse.text)
+saveJson = json.loads(saveResponse.text)
+print(saveJson['m'])
